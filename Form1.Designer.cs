3360e61a103540244164885d462b3d13735271a8
Genshin804 <doris8ch@gmail.com>
2022-06-24 14:25:48 +0800

    Roye

---
+++ Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿
+namespace EnCodingChange
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btn_Change = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.txtMsg = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtExtension = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtKeyword = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(11, 34);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "來源路徑";
+            // 
+            // txtPath
+            // 
+            this.txtPath.Location = new System.Drawing.Point(81, 30);
+            this.txtPath.Margin = new System.Windows.Forms.Padding(2);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(321, 23);
+            this.txtPath.TabIndex = 1;
+            this.txtPath.Text = "D:\\SteamLibrary\\steamapps\\workshop\\content\\108600";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(422, 30);
+            this.button1.Margin = new System.Windows.Forms.Padding(2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(73, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "瀏覽";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btn_Change
+            // 
+            this.btn_Change.Location = new System.Drawing.Point(422, 65);
+            this.btn_Change.Margin = new System.Windows.Forms.Padding(2);
+            this.btn_Change.Name = "btn_Change";
+            this.btn_Change.Size = new System.Drawing.Size(73, 23);
+            this.btn_Change.TabIndex = 3;
+            this.btn_Change.Text = "轉編碼";
+            this.btn_Change.UseVisualStyleBackColor = true;
+            this.btn_Change.Click += new System.EventHandler(this.btn_Change_Click);
+            // 
+            // txtMsg
+            // 
+            this.txtMsg.Location = new System.Drawing.Point(11, 130);
+            this.txtMsg.Margin = new System.Windows.Forms.Padding(2);
+            this.txtMsg.Multiline = true;
+            this.txtMsg.Name = "txtMsg";
+            this.txtMsg.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtMsg.Size = new System.Drawing.Size(391, 165);
+            this.txtMsg.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(12, 313);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(169, 15);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "本程式僅進行Big5編碼轉UTF8";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 65);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 15);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "檔案副檔名";
+            // 
+            // txtExtension
+            // 
+            this.txtExtension.Location = new System.Drawing.Point(81, 62);
+            this.txtExtension.Name = "txtExtension";
+            this.txtExtension.Size = new System.Drawing.Size(100, 23);
+            this.txtExtension.TabIndex = 8;
+            this.txtExtension.Text = "txt";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.Color.Red;
+            this.label4.Location = new System.Drawing.Point(187, 65);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(118, 15);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "複數副檔名請以,區隔";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 100);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(67, 15);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "檔案關鍵字";
+            // 
+            // txtKeyword
+            // 
+            this.txtKeyword.Location = new System.Drawing.Point(81, 97);
+            this.txtKeyword.Name = "txtKeyword";
+            this.txtKeyword.Size = new System.Drawing.Size(100, 23);
+            this.txtKeyword.TabIndex = 11;
+            this.txtKeyword.Text = "_CH";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(512, 352);
+            this.Controls.Add(this.txtKeyword);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtExtension);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtMsg);
+            this.Controls.Add(this.btn_Change);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtPath);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btn_Change;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.TextBox txtMsg;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtExtension;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtKeyword;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EnCodingChange
+{
+    public partial class Form1 : Form
+    {        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.ShowDialog();
+            txtPath.Text = folderBrowserDialog1.SelectedPath;
+        }
+
+        private void btn_Change_Click(object sender, EventArgs e)
+        {
+            List<string> ResultList = new List<string>();
+            string strPath = txtPath.Text;
+
+            if (string.IsNullOrEmpty(strPath))
+            {
+                MessageBox.Show("請填寫路徑");
+                return;
+            }
+            GetDirectory(strPath);
+        }
+
+        /// <summary>
+        /// 檢查檔案編碼
+        /// </summary>
+        /// <param name="path">路徑</param>
+        /// <returns></returns>
+        public string CheckEncoding(string path)
+        {
+            string vstrFormFile = path;
+            using (FileStream fs = File.Open(vstrFormFile, FileMode.Open, FileAccess.Read, FileShare.Read))
+            {
+                using (StreamReader sr = new StreamReader(fs, Encoding.GetEncoding(950), true))
+                {
+                    sr.Read();
+                    sr.Close();
+
+                    return sr.CurrentEncoding.BodyName.ToString();
+                }
+                fs.Close();                
+            }           
+        }
+
+        private void GetDirectory(string path)
+        {
+            List<string> ResultList = new List<string>();
+            DirectoryInfo d = new DirectoryInfo(path);
+
+            string strExtension = this.txtExtension.Text;
+            string strKeyword = this.txtKeyword.Text;
+
+            var files = d.GetFiles().Where(x => x.Extension.Contains(strExtension) && x.Name.Contains(strKeyword)).ToList();
+            //MessageBox.Show(files.Count().ToString());
+            if(files.Count() != 0)
+            {
+                this.txtMsg.Text += "當前發現需轉換檔案：" + files.Count().ToString() + "個\r\n";
+
+                foreach(var file in files)
+                {
+                    var type = CheckEncoding(file.FullName);
+
+                    this.txtMsg.Text += "檔案：" + file.FullName.ToString() + "\r\n編碼:" + type;
+
+                    if (type.ToUpper().Contains("BIG5"))
+                    {
+                        ChangeEncoding(file.FullName, Encoding.GetEncoding(950));
+
+                        this.txtMsg.Text += "\r\n已轉換 UTF8";
+                    }
+                    else
+                    {
+                        this.txtMsg.Text += "\r\n未轉換";
+                    }
+                }                
+            }
+            else
+            {
+                this.txtMsg.Text += "無需要轉換的檔案\r\n";
+            }
+        }
+
+        private void ChangeEncoding(string filename, Encoding encoding)
+        {
+            FileStream fs = new FileStream(filename, FileMode.Open, FileAccess.Read);
+            byte[] flieByte = new byte[fs.Length];
+            fs.Read(flieByte, 0, flieByte.Length);
+            fs.Close();
+
+            StreamWriter docWriter;
+            Encoding ec = Encoding.GetEncoding("UTF-8");
+            docWriter = new StreamWriter(filename, false, ec);
+            docWriter.Write(encoding.GetString(flieByte));
+            docWriter.Close();
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+            //var a = CodePagesEncodingProvider.Instance.GetEncoding(950);
+            //var b = Encoding.GetEncoding(950);
+            //this.txtMsg.Text += $"Name={b.EncodingName}, CodePage={b.CodePage}";
+
+            //var encodings = Encoding.GetEncodings();
+
+            //foreach(var item in encodings)
+            //{
+            //    Console.WriteLine($"DisplayNameite={item.DisplayName}, CodePage={item.CodePage}");
+            //    txtMsg.Text += $"DisplayNameitem={item.DisplayName}, CodePage={item.CodePage} \r\n";
+            //}
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
+
+            var encodings = Encoding.GetEncodings();
+
+            foreach (var item in encodings)
+            {
+                txtMsg.Text += $"DisplayNameitem={item.DisplayName}, CodePage={item.CodePage} \r\n";
+            }
+
+            txtMsg.Text += $"Count={encodings.Count()}";
+        }
+    }
+}
